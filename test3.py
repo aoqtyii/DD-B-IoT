53f74dd666638ab751f6853bfcc6c3043fec70db
aoqtyii <aoqtyii@163.com>
2020-12-19 20:57:22 +0800

    register of block_chain_1 PPP number_of_nodes unsolved

--- test3.py
+++ test3.py
@@ -5,6 +5,7 @@
 @Date : 2020/6/14
 @Desc :
 """
+from collections import Counter
 
 import numpy as np  # NumPy package for arrays, random number generation, etc
 import matplotlib.pyplot as plt  # For plotting
@@ -13,78 +14,80 @@ from scipy import integrate  # For integrating
 
 
 def plot1(s):
-    plt.close('all')  # close all figures
+    plt.close('all')  # 关闭所有绘图
 
-    # Simulation window parameters
+    # 模拟的窗口为矩形
     xMin = -1
     xMax = 1
     yMin = -1
     yMax = 1
     xDelta = xMax - xMin
-    yDelta = yMax - yMin  # rectangle dimensions
+    yDelta = yMax - yMin
     areaTotal = xDelta * yDelta
 
-    numbSim = 10  # number of simulations
+    numbPointsRetained = 21
 
-    # s = 0.8  # scale parameter
+    # s = 0.8  # 比例参数
 
-    # Point process parameters
+    # 点过程参数
     def fun_lambda(x, y):
-        return 10*np.exp(-(x ** 2 + y ** 2) / s ** 2)  # intensity function
+        return 10 * np.exp(-(x ** 2 + y ** 2) / s ** 2)  # 强度函数
 
-    ###START -- find maximum lambda -- START ###
-    # For an intensity function lambda, given by function fun_lambda,
-    # finds the maximum of lambda in a rectangular region given by
-    # [xMin,xMax,yMin,yMax].
+    # START -- 找到最大的强度 -- START
+    # 对任意的强度函数lambda，找到矩形区域内最大的lambda
     def fun_Neg(x):
         m = -fun_lambda(x[0], x[1])
-        return m  # negative of lambda
+        return m  # 负的lambda
 
-    xy0 = [(xMin + xMax) / 2, (yMin + yMax) / 2]  # initial value(ie centre)
-    # Find largest lambda value
+    xy0 = [(xMin + xMax) / 2, (yMin + yMax) / 2]  # 初始值(中心位置值)
+    # 找到最大的lambda值
     resultsOpt = minimize(fun_Neg, xy0, bounds=((xMin, xMax), (yMin, yMax)))
-    lambdaNegMin = resultsOpt.fun  # retrieve minimum value found by minimize
+    lambdaNegMin = resultsOpt.fun  # 通过minimize函数找到最小值
     lambdaMax = -lambdaNegMin
 
-    ###END -- find maximum lambda -- END ###
+    # END -- 找到最大的lambda值 -- END
 
-    # define thinning probability function
+    # 定义 thinning prob 函数
     def fun_p(x, y):
         return fun_lambda(x, y) / lambdaMax
 
     # fun_p = lambda x, y: fun_lambda(x, y) / lambdaMax;
+    # numbPointsRetained = np.zeros(numbSim)
 
-    # for collecting statistics -- set numbSim=1 for one simulation
-    numbPointsRetained = np.zeros(numbSim)  # vector to record number of points
-    # Simulate a Poisson point process
-    numbPoints = np.random.poisson(areaTotal * lambdaMax)  # Poisson number of points
-    xx = np.random.uniform(0, xDelta, (numbPoints, 1)) + xMin  # x coordinates of Poisson points
-    yy = np.random.uniform(0, yDelta, (numbPoints, 1)) + yMin  # y coordinates of Poisson points
+    # 模拟泊松点过程
+    while True:
+        numbPoints = np.random.poisson(areaTotal * lambdaMax)  # 泊松分布的点个数
+        if numbPoints > numbPointsRetained:
+            xx = np.random.uniform(0, xDelta, (numbPoints, 1)) + xMin  # 泊松分布点坐标的x值
+            yy = np.random.uniform(0, yDelta, (numbPoints, 1)) + yMin  # 泊松分布点坐标的y值
 
-    # calculate spatially-dependent thinning probabilities
-    p = fun_p(xx, yy)
+            # 计算空间独立的thinning prob.
+            p = fun_p(xx, yy)
 
-    # Generate Bernoulli variables (ie coin flips) for thinning
-    booleRetained = np.random.uniform(0, 1, (numbPoints, 1)) < p  # points to be retained
+            # 为thinning生成伯努利变量
+            booleRetained = np.random.uniform(0, 1, (numbPoints, 1)) < p  # 被保留的点
 
-    # x/y locations of retained points
-    xxRetained = xx[booleRetained]
-    yyRetained = yy[booleRetained]
+            c = Counter(booleRetained.T[0]).get(True)
+
+            # 保留点的坐标
+            xxRetained = xx[booleRetained]
+            yyRetained = yy[booleRetained]
+            break
 
     def plot():
-        # Plotting
+        # 绘图
         plt.scatter(xxRetained, yyRetained, edgecolor='b', facecolor='none', alpha=0.5)
         plt.xlabel('x')
         plt.ylabel('y')
         plt.show()
 
         # run empirical test on number of points generated
-        if numbSim >= 10:
-            # total mean measure (average number of points)
-            LambdaNumerical = integrate.dblquad(fun_lambda, xMin, xMax, lambda x: yMin, lambda y: yMax)[0]
-            # Test: as numbSim increases, numbPointsMean converges to LambdaNumerical
-            numbPointsMean = np.mean(numbPointsRetained)
-            # Test: as numbSim increases, numbPointsVar converges to LambdaNumerical
-            numbPointsVar = np.var(numbPointsRetained)
+        # if numbSim >= 10:
+        #     # total mean measure (average number of points)
+        #     LambdaNumerical = integrate.dblquad(fun_lambda, xMin, xMax, lambda x: yMin, lambda y: yMax)[0]
+        #     # Test: as numbSim increases, numbPointsMean converges to LambdaNumerical
+        #     numbPointsMean = np.mean(numbPointsRetained)
+        #     # Test: as numbSim increases, numbPointsVar converges to LambdaNumerical
+        #     numbPointsVar = np.var(numbPointsRetained)
 
     plot()
